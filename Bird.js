b540fb4d5b16dbeae0d6f9a155bf0214bf3cf0c6
rishan-bheda <74193392+rishan-bheda@users.noreply.github.com>
2021-02-02 20:13:25 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,34 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+
+    this.trajectory = [];
+    
+  }
+
+  display() {
+
+    super.display();
+
+    console.log(this.body);
+
+    if(this.body.velocity.x > 8 && this.body.position.x > 200){
+
+      var position = [this.body.position.x, this.body.position.y];
+    
+      this.trajectory.push(position);
+
+    }
+
+    for(var i = 0; i < this.trajectory.length; i++){
+
+      image(this.smoke, this.trajectory[i][0],this.trajectory[i][1]);
+
+    }
+
+    
+    
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Angry_Bird-6
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
